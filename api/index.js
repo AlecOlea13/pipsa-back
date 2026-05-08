bc6b81897cdfba2b366a152ae98c5c1b1a2067a9
AlecOlea13 <2122100694@soy.utj.edu.mx>
2026-05-07 20:31:57 -0600

    cambio de index js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import app, { connectDB } from '../server.js';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://last-to-do-u9vd.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
+
   await connectDB();
   return app(req, res);
 }
