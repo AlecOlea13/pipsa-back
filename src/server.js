c2dffca62e9e708a1839472295789f1e7028d47a
Alec <2122100694@soy.utj.edu.mx>
2026-06-15 12:43:00 -0600

    Error pago multiple

--- src/server.js
+++ src/server.js
@@ -37,7 +37,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => res.json({ ok: true, name: 'Control Pipsa API' }));
