5452845d5869d4f4403e558e35e1313c654b5d48
Alec <2122100694@soy.utj.edu.mx>
2026-07-16 10:59:01 -0600

    fixes

--- src/routes/cxc.routes.js
+++ src/routes/cxc.routes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import { getCxcs, createCxc, updateCxc, deleteCxc, cobrarCxc, cobrarPorRep } from "../controllers/cxc.controller.js";
+import { getCxcs, createCxc, updateCxc, deleteCxc, cobrarCxc, cobrarPorRep, cobrarMultiple } from "../controllers/cxc.controller.js";
 import { auth, requireRol } from "../middleware/auth.js";
 
 const router = Router();
 const canAccess = requireRol("developer", "gerencia", "oficina");
 
-router.get("/",             auth, canAccess, getCxcs);
-router.post("/",            auth, canAccess, createCxc);
+router.get("/",                auth, canAccess, getCxcs);
+router.post("/",               auth, canAccess, createCxc);
 router.post("/cobrar-por-rep", auth, canAccess, cobrarPorRep);
-router.put("/:id",          auth, canAccess, updateCxc);
-router.delete("/:id",       auth, requireRol("developer", "gerencia"), deleteCxc);
-router.post("/:id/cobrar",  auth, canAccess, cobrarCxc);
+router.post("/cobrar-multiple", auth, canAccess, cobrarMultiple);
+router.put("/:id",             auth, canAccess, updateCxc);
+router.delete("/:id",          auth, requireRol("developer", "gerencia"), deleteCxc);
+router.post("/:id/cobrar",     auth, canAccess, cobrarCxc);
 
 export default router;
