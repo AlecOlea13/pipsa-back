fdb7cc0bcc43610d84268051f72666792f7126a2
Alec <2122100694@soy.utj.edu.mx>
2026-09-02 16:57:42 -0600

    Ya fix

--- src/routes/servicio.routes.js
+++ src/routes/servicio.routes.js
@@ -12,11 +12,11 @@ router.get('/',                               auth, getServicios);
 router.get('/por-montacargas/:montacargasId', auth, getServiciosPorMontacargas);
 router.get('/:id',                            auth, getServicio);
 router.post('/',                              auth, createServicio);
-router.put('/:id',                            auth, requireRol(["developer","gerencia","supervisor_almacen"]), updateServicio);
+router.put('/:id',                            auth, requireRol("developer","gerencia","supervisor_almacen"), updateServicio);
 router.post('/:id/iniciar',                   auth, iniciarServicio);
 router.post('/:id/pausar',                    auth, pausarServicio);
 router.post('/:id/reanudar',                  auth, reanudarServicio);
 router.post('/:id/cerrar',                    auth, cerrarServicio);
-router.delete('/:id',                         auth, requireRol(["developer"]), eliminarServicio);
+router.delete('/:id',                         auth, requireRol("developer"), eliminarServicio);
 
 export default router;
